94d7e53d92a0e2c288280040632273ca9afff6f6
luotianxiang <luotianxiang@youzan.com>
2018-10-31 17:42:40 +0800

    m

--- src/main/java/com/java/com/java/learn/netty/ClientHandler.java
+++ src/main/java/com/java/com/java/learn/netty/ClientHandler.java
@@ -1,10 +1,14 @@
 package com.java.com.java.learn.netty;
 
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
  * User: tianxiang.luo
  * Date: 2018-10-09 下午8:07
  */
-public class ClientHandler {
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+
 }
--- src/main/java/com/java/com/java/learn/netty/NettyClient.java
+++ src/main/java/com/java/com/java/learn/netty/NettyClient.java
@@ -1,5 +1,10 @@
 package com.java.com.java.learn.netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -7,4 +12,15 @@ package com.java.com.java.learn.netty;
  * Date: 2018-10-09 下午8:07
  */
 public class NettyClient {
+
+
+    public void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) {
+                ch.pipeline().addLast(new ClientHandler());
+            }
+        });
+    }
 }
