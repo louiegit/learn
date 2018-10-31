8032d4472bd4e59190ad8e59e379b11067723af9
luotianxiang <luotianxiang@youzan.com>
2018-10-31 23:42:10 +0800

    002

--- src/main/java/com/java/LearnEverythingApplication.java
+++ src/main/java/com/java/LearnEverythingApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnEverythingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("----1-");
-		System.out.println("---33332-");
 		SpringApplication.run(LearnEverythingApplication.class, args);
 	}
 }
